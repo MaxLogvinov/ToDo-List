ea3aa7a175c4659fadc6eb15e46d284db7e10f9d
Max Logvinov <maxlogvinov93@gmail.com>
2024-12-23 20:06:07 +0300

    add loading and error handler

---
+++ src/components/Loading.tsx
@@ -0,0 +1,6 @@
+// import styles from './Loading.module.scss';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function Loading() {
+  return <CircularProgress size="3rem" />;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,28 @@ import { useEffect } from 'react';
 import TodosList from '@/components/TodosList';
 import { RootState } from '@/servises/store';
 import { hydrateTodos } from '../servises/slices/todosSlice';
+import Loading from '../components/Loading';
 
 export default function Home() {
   const dispatch = useDispatch<AppDispatch>();
-  const todos = useSelector((state: RootState) => state.todos.todos);
+  const { loading, error, todos } = useSelector((state: RootState) => state.todos);
 
   useEffect(() => {
     const storedTodos = localStorage.getItem('todos');
     if (storedTodos) {
       const parsedTodos = JSON.parse(storedTodos);
       dispatch(hydrateTodos(parsedTodos));
-    } else {
+    } else if (todos.length < 1) {
       dispatch(getTodos());
     }
   }, [dispatch, todos.length]);
+
+  if (loading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <div>Сервер временно недоступен. Попробуйте позже.</div>;
+  }
 
   return (
     <>
